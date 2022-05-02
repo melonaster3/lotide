2ab29e9743ffc72bb7af9660d98028dbfd891af7
Sang Lee <melonaster@gmail.com>
2022-05-02 23:32:23 +0000

    added the tail

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+    return console.log(`👎👎👎👎👎Assertion has failed ${actual} !== ${expected}`);
+  } else {
+    return console.log(`😉😉😉😉😉 Assertion has passed ${actual} === ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  if (array.length === 0 || array.length === []) {
+    let empty = [];
+    return empty;
+  } else {
+    return array.slice(1,array.length);
+  }
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
