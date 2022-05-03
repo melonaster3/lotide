e3a383e109704bfb0b9f51334fa591111f39b82e
Sang Lee <melonaster@gmail.com>
2022-05-03 21:49:23 +0000

    Fixed for loop in eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,27 +7,30 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function (array1, array2) {
-  let result; 
+const eqArrays = function(array1, array2) {
+  let result;
   if (array1.length !== array2.length) {
     result = false;
     return result;
-  }
-  for(let num1 of array1) {
-    if(array1[num1] !== array2[num1]) {
-       result = false;
-       return result;
-    } else { 
-       result = true;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+        return result;
+      } else {
+        result = true;
+      }
     }
-}
-return result;
-}
+    return result;
+  }
+};
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+/*
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+*/
