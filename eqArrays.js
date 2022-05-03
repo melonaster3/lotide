b0802949798e1e42c3bb0392953ff44a1a6ab0cd
Sang Lee <melonaster@gmail.com>
2022-05-03 20:47:05 +0000

    Finished eqArrays.js coding

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+    return console.log(`👎👎👎👎👎Assertion has failed ${actual} !== ${expected}`);
+  } else {
+    return console.log(`😉😉😉😉😉 Assertion has passed ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  let result; 
+  for(let num1 of array1) {
+    if(array1[num1] !== array2[num1]) {
+       result = false;
+       return result;
+    } else { 
+       result = true;
+    }
+}
+return result;
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
