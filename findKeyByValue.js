6fb962086f9df22facf7a3302dc8350f4d333e7f
Sang Lee <melonaster@gmail.com>
2022-05-04 22:45:32 +0000

    Finished findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+    return console.log(`👎👎👎👎👎Assertion has failed ${actual} !== ${expected}`);
+  } else {
+    return console.log(`😉😉😉😉😉 Assertion has passed ${actual} === ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function (guide, want) {
+  let keys = Object.keys(guide);
+  let value = undefined;
+  for (let i = 0; i < keys.length; i++) {
+    if (guide[keys[i]] === want) {
+      value = keys[i];
+    }
+  }  
+  return value;
+}
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
