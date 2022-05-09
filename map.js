e20f7cddd792c05fd64cb3be5626693f78efe359
Sang Lee <melonaster@gmail.com>
2022-05-09 22:37:08 +0000

    Added map.js

--- map.js
+++ map.js
@@ -8,7 +8,7 @@ for (let item of array) {
 }
 return results;
 }
-
+/*
 const words = ["ground", "control", "to", "major", "tom"];
 const words1 = ["sang", "likes", "to", "coding"];
 const words2 = ["what", "is", "going", "on", "major", "tom"];
@@ -22,5 +22,5 @@ const results3 = map(words2, word => word[0]);
 assertArraysEqual(results1,["g",'c',"t","m","t"]);
 assertArraysEqual(results2,["s",'l',"t","c"]);
 assertArraysEqual(results3,["w",'i',"g","o","m","t"]);
-
+*/
 module.exports = map;
