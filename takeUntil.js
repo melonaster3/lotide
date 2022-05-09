19cabbb2bcfb1d73b549cfb5face69130d24daea
Sang Lee <melonaster@gmail.com>
2022-05-09 22:38:54 +0000

    Edited takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -12,7 +12,7 @@ let newArray =[];
   return newArray;
 }
 
-
+/*
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -24,6 +24,6 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 assertArraysEqual(results2,["I've", "been", "to", "Hollywood"]);
 console.log(results2);
-
+*/
 
 module.exports = takeUntil;
