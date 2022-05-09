32c8a8bb6b0078fb35cff4a96a9fa5b9367dad29
Sang Lee <melonaster@gmail.com>
2022-05-09 20:47:08 +0000

    added headtest

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,22 @@
-const assertEqual = require('../assertEqual');
+/*const assertEqual = require('../assertEqual');
 const head = require('../head')
 
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([], undefined));
+assertEqual(head([], undefined)); */
+
+const assert = require('chai').assert; 
+const head = require('../head');
+
+describe ('#head', () => {
+  it("Function should return 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]),5);
+  }),
+  it("Function should return Hello for ['Hello','Lighthouse','Labs]", () => {
+    assert.strictEqual(head(['Hello','Lighthouse','Labs']),'Hello');
+  }),
+  it("Function should return undefined for []", () => {
+    assert.strictEqual(head([]),undefined);
+  });
+});
